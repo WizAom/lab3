b4005a97fa3f5ce33ddef76a20074d955403c435
WizAom <Aom_ll2555@hotmail.com>
2019-12-30 14:43:52 +0700

    first version

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
-int main()
+main()
 {
-    float a,b,c;
-    cout << "A";
+	float a=2,b,c=0;
+    while(a<=69){
+        c=c+(1/a);
+    	a++;
+    }
+    cout << c;
     return 0;
-
 }
